c25c0d2f51458dcf55d68a9436b2dd26e26b3eba
Lucas Filipe Cordova <lucascordova1000@gmail.com>
2025-01-23 15:37:49 -0300

    fixed AOS issues

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -8,13 +8,13 @@ import ContactForm from './ContactForm'
 const Contact = () => {
     return (
         <div id="contact" className='bg-black pt-20 pb-20'>
-            <div className='flex justify-center flex-col w-[90%] sm:w-[80%] h-full mx-auto '>
+            <div className='flex justify-center flex-col w-[90%] sm:w-[80%] h-full mx-auto bg-black'>
                 <h1 className="flex items-center justify-center">
                     <span className="flex-grow border-t border-white"></span>
                 </h1>
-                <div data-aos="fade-up" className='grid grid-cols-1 lg:grid-cols-2 items-center gap-12 pt-20 pb-15'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 items-center gap-12 pt-20 pb-15 bg-black'>
                     {/*Text Content*/}
-                    <div>
+                    <div data-aos="fade-up">
                         <h1 className=' text-white text-4xl sm:text-4xl md:text-5xl mt-6 mb-6 font-bold md:leading-[3rem]
                             lg:leading[3.5rem]'>Let&apos;s <span className='text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-blue-500'>Connect!
                             </span>
--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="bg-black">
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 bg-black">
                 <hr className="my-6 border-gray-200  dark:border-gray-700 lg:my-8" />
                 <span className="block text-sm text-white sm:text-center dark:text-gray-400">© 2025 Lucas Cordova. All Rights Reserved.</span>
             </div>
